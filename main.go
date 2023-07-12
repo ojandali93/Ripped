9e6c3d4a40aa6d69ba8d2fa6fbb16e85c8fc4d0e
omar jandali <omarjandali93@gmail.com>
2023-07-12 15:20:10 -0700

    Omar Jandali. Made adjustments to rapid api request response object

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"io"
+	// "io"
 	"github.com/gorilla/mux"
 )
 
@@ -32,8 +33,6 @@ func generalSearch(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("City:", city)
 	fmt.Println("State:", state)
 
-	fmt.Fprintf(w, "Search results for City: %s, State: %s", city, state)
-
 	url := "https://zillow56.p.rapidapi.com/search?location=" + city + "%2C%20" + state
 	fmt.Println("url:", url)
 
@@ -46,7 +45,20 @@ func generalSearch(w http.ResponseWriter, r *http.Request) {
 	res, _ := http.DefaultClient.Do(req)
 
 	defer res.Body.Close()
-	body, _ := io.ReadAll(res.Body)
+	var responseBody struct {
+		Results []struct {
+			Bedrooms float64 `json:"bedrooms"`
+			StreetAddress string `json:"streetAddress"`
+			// Add other fields as needed to match the response structure
+		} `json:"results"`
+	}
 
-	fmt.Fprintf(w, "Results: ", string(body))
+	err := json.NewDecoder(res.Body).Decode(&responseBody)
+	if err != nil {
+		// Handle error here
+	}
+
+	for _, property := range responseBody.Results {
+		fmt.Println("Property:", property.StreetAddress)
+	}
 }
